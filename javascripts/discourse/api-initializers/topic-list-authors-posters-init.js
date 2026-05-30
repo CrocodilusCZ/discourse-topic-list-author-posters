0b64e7b0978c8efebe9a143eea8fa130dde32764
CrocodilusCZ <56224782+CrocodilusCZ@users.noreply.github.com>
2026-05-30 12:30:13 +0200

    úprava autora ikonky

--- javascripts/discourse/api-initializers/topic-list-authors-posters-init.js
+++ javascripts/discourse/api-initializers/topic-list-authors-posters-init.js
@@ -1,18 +1,8 @@
 import { apiInitializer } from "discourse/lib/api";
 import CustomPostersHeader from "../components/custom-posters-header";
 import CustomPostersCell from "../components/custom-posters-cell";
-import CustomCategoriesLatestAvatar from "../components/custom-categories-latest-avatar";
 
-// Show topic author avatar on categories page if setting enabled
 export default apiInitializer((api) => {
-  if (settings.show_author_on_categories_page === true) {
-    api.renderInOutlet(
-      "latest-topic-list-item-topic-poster",
-      CustomCategoriesLatestAvatar
-    );
-  }
-
-  // Replace posters header AND poster column contents in topic lists if setting enabled
   api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
     if (settings.topic_list_show_last_poster_only === true) {
       columns.replace("posters", {
